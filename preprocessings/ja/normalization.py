32f706269c3e54039e4b5b485a84bbce2cfe7066
Hironsan <light.tree.1.13@gmail.com>
2017-04-03 15:08:20 +0900

    add lemmatizer

--- preprocessings/ja/normalization.py
+++ preprocessings/ja/normalization.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import unicodedata
 
+import nltk
+from nltk.corpus import wordnet
+
 
 def lower_text(text):
     return text.lower()
@@ -9,3 +12,14 @@ def lower_text(text):
 def normalize_unicode(text, form='NFKC'):
     normalized_text = unicodedata.normalize(form, text)
     return normalized_text
+
+
+def lemmatize_term(term, pos=None):
+    if pos is None:
+        synsets = wordnet.synsets(term)
+        if not synsets:
+            return term
+        pos = synsets[0].pos()
+        if pos == wordnet.ADJ_SAT:
+            pos = wordnet.ADJ
+    return nltk.WordNetLemmatizer().lemmatize(term, pos=pos)
