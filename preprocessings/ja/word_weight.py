2044feb9ee9a3bce98f909deea8dfca9680b9ceb
h-nakayama <light.tree.1.13@gmail.com>
2017-04-04 22:06:12 +0900

    implement BoW and TF-IDF

--- preprocessings/ja/word_weight.py
+++ preprocessings/ja/word_weight.py
@@ -1,24 +1,19 @@
-import json
-import os
-from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
-from sklearn.model_selection import train_test_split
+# -*- coding:utf-8 -*-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
-file_name = os.path.join(os.path.dirname(__file__), '../../data/processed/livedoor_tokenized.json')
-with open(file_name) as f:
-    livedoor = json.load(f)
+corpus = [
+	'This is a pen.',
+	'That is a bot.',
+	'These are red document and blue document.',
+]
 
-docs = livedoor['data']
-docs = [' '.join(d) for d in docs]
-X_train, X_test, y_train, y_test = train_test_split(docs, livedoor['label'], test_size=0.4, random_state=0)
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names())
+print(X.toarray())
 
-#tfidf_transformer = TfidfTransformer()
-#X_train_tfidf = tfidf_transformer.fit_transform(texts)
 vectorizer = TfidfVectorizer()
-X_train = vectorizer.fit_transform(docs)
-print(X_train.shape)
-print(y_train.shape)
-from sklearn.naive_bayes import MultinomialNB
-text_clf = MultinomialNB().fit(X_train, y_train)
-print(text_clf.score(X_test, y_test))
-
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names())
+print(X.toarray())
