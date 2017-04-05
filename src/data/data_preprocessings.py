aae8d8a2a192344579b02610df6bf5444e016b1f
Hironsan <light.tree.1.13@gmail.com>
2017-04-06 08:48:33 +0900

    change cleaning function
    don't remove characters between [] and () and so on.

--- src/data/data_preprocessings.py
+++ src/data/data_preprocessings.py
@@ -14,9 +14,9 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), '../../data/processed')
 def cleaning(text):
     replaced_text = '\n'.join(s.strip() for s in text.split('\n')[2:] if s != '')  # skip header by [2:]
     replaced_text = re.sub(r'\d+', '0', replaced_text)     # 数字の置き換え
-    replaced_text = re.sub(r'【.*?】', '', replaced_text)   # 【】の除去
-    replaced_text = re.sub(r'（.*?）', '', replaced_text)   # （）の除去
-    replaced_text = re.sub(r'［.*?］', '', replaced_text)   # ［］の除去
+    replaced_text = re.sub(r'[【】]', ' ', replaced_text)   # 【】の除去
+    replaced_text = re.sub(r'[（）]', ' ', replaced_text)   # （）の除去
+    replaced_text = re.sub(r'[［］]', ' ', replaced_text)   # ［］の除去
     replaced_text = re.sub(r'[@＠]\w+', '', replaced_text)  # メンションの除去
     replaced_text = re.sub(r'https?:\/\/.*?[\r\n ]', '', replaced_text)  # URLの除去
     replaced_text = re.sub(r'　', '', replaced_text)  # 全角空白の除去
