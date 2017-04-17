8a04820ab981272b2469c0de0a81bac8c2dbfd3f
h-nakayama <light.tree.1.13@gmail.com>
2017-04-17 20:55:39 +0900

    bug fix: tokenizer

--- preprocessings/ja/tokenizer.py
+++ preprocessings/ja/tokenizer.py
@@ -20,21 +20,12 @@ class JanomeTokenizer(object):
         return words
 
     def tokenize(self, sent):
+        token = namedtuple('Token', 'surface, pos, pos_detail1, pos_detail2, pos_detail3,\
+                                             infl_type, infl_form, base_form, reading, phonetic')
         for t in self._t.tokenize(sent):
-            token = namedtuple('Token', 'surface, pos, pos_detail1, pos_detail2, pos_detail3,\
-                                                 infl_type, infl_form, base_form, reading, phonetic')
             poses = t.part_of_speech.split(',')
-            token.surface = t.surface        # 表層形
-            token.pos = poses[0]             # 品詞
-            token.pos_detail1 = poses[1]     # 品詞細分類1
-            token.pos_detail2 = poses[2]     # 品詞細分類2
-            token.pos_detail3 = poses[3]     # 品詞細分類3
-            token.infl_type = t.infl_type    # 活用型
-            token.infl_form = t.infl_form    # 活用形
-            token.base_form = t.base_form    # 原型
-            token.reading = t.reading        # 読み
-            token.phonetic = token.phonetic  # 発音
-            yield token
+            yield token(t.surface, poses[0], poses[1], poses[2], poses[3],
+                        t.infl_type, t.infl_form, t.base_form, t.reading, t.phonetic)
 
     def filter_by_pos(self, sent, pos=('名詞', )):
         tokens = [token for token in self.tokenize(sent) if token.pos in pos]
@@ -77,5 +68,5 @@ class MeCabTokenizer(object):
             yield token(surface, *feature)
 
     def filter_by_pos(self, sent, pos=('名詞',)):
-        tokens = [token for token in self.tokenize(sent) if token.pos == pos]
+        tokens = [token for token in self.tokenize(sent) if token.pos in pos]
         return tokens
