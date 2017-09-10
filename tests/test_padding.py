bd26c196fc87b3c920d2d87b3fa46d1fdcdf7a7b
h-nakayama <light.tree.1.13@gmail.com>
2017-09-10 09:02:16 +0900

    Add test case for pad_char_sequences

--- tests/test_padding.py
+++ tests/test_padding.py
@@ -60,8 +60,34 @@ class TestPadding(unittest.TestCase):
                             [[3, 1], [3, 2], [3, 3]]])
 
     def test_pad_char_sequences(self):
-        a = [[[1, 2, 3, 4], [1, 2], [1], [1, 2, 3]],
-             [[1, 2, 3, 4, 5], [1, 2], [1, 2, 3, 4]]]
+        a = [[[1]],
+             [[2], [2, 2]],
+             [[3], [3, 2], [3, 3, 3]]]
+
+        # test padding
+        b = pad_char_sequences(a, padding='pre')
+        assert_allclose(b, [[[0, 0, 0], [0, 0, 0], [0, 0, 1]],
+                            [[0, 0, 0], [0, 0, 2], [0, 2, 2]],
+                            [[0, 0, 3], [0, 3, 2], [3, 3, 3]]])
+
         b = pad_char_sequences(a, padding='post')
-        assert_allclose(b, [[[1, 2, 3, 4, 0], [1, 2, 0, 0, 0], [1, 0, 0, 0, 0], [1, 2, 3, 0, 0]],
-                            [[1, 2, 3, 4, 5], [1, 2, 0, 0, 0], [1, 2, 3, 4, 0], [0, 0, 0, 0, 0]]])
+        assert_allclose(b, [[[1, 0, 0], [0, 0, 0], [0, 0, 0]],
+                            [[2, 0, 0], [2, 2, 0], [0, 0, 0]],
+                            [[3, 0, 0], [3, 2, 0], [3, 3, 3]]])
+
+        # test truncating
+        b = pad_char_sequences(a, maxlen=2, padding='pre')
+        assert_allclose(b, [[[0, 0], [0, 0], [0, 1]],
+                            [[0, 0], [0, 2], [2, 2]],
+                            [[0, 3], [3, 2], [3, 3]]])
+
+        b = pad_char_sequences(a, maxlen=2, padding='post')
+        assert_allclose(b, [[[1, 0], [0, 0], [0, 0]],
+                            [[2, 0], [2, 2], [0, 0]],
+                            [[3, 0], [3, 2], [3, 3]]])
+
+        # test value
+        b = pad_char_sequences(a, padding='pre', value=1)
+        assert_allclose(b, [[[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+                            [[1, 1, 1], [1, 1, 2], [1, 2, 2]],
+                            [[1, 1, 3], [1, 3, 2], [3, 3, 3]]])
